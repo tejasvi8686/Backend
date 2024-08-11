3b1bd3a8976e7d9020e7a6528240758472951678
tejavi8686 <tejasviraj8686@gmail.com>
2024-08-11 22:53:29 +0530

    backend chai code

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express();
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/twitter' , (req, res) => {
+    res.send('tejasviraj8686.com')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1> please login at chai aur code </h1>')
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${port}`);
+    
+})
